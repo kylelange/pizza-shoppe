bf7f256a64a8fcba72fe02ee4f8c8e93907248b5
Kyle Lange <baronsintrees@gmail.com>
2016-08-25 23:24:54 -0700

    add object and variables for user input to use in the object.

--- pizza-shoppe/js/scripts.js
+++ pizza-shoppe/js/scripts.js
@@ -1,11 +1,22 @@
 // Business Logic
 
-Pizza
+function Pizza (name, topping, size) {
+  this.name = name;
+  this.topping = topping;
+  this.size = size;
+}
 
+Pizza.prototype.cost = function() {
+
+}
 
 // User Interface Logic
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+    var inputName = $("").val();
+    var inputTopping = $("").val();
+    var inputSize = $("").val();
+    var newPizza = new Pizza(inputName, inputTopping, inputSize);
   });
 });
